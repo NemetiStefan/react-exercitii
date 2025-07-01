46c69749509a34badd3893cf5020bbd3929f6b93
NemetiStefan <stefval112@gmail.com>
2025-07-01 10:54:25 +0300

    ex1.2

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -3,6 +3,14 @@ const Header = () => {
   return (<div><h1>{course}</h1></div>)
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -13,27 +21,32 @@ const Content = () => {
 
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
   
 }
 
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
       <Header />
       <Content />
+      <Total />
     </div>
   )
 
