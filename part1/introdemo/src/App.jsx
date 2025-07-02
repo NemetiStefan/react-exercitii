8d63f00fbfd4c5dc579ae95043c570e8ce263db8
NemetiStefan <stefval112@gmail.com>
2025-07-02 09:58:39 +0300

    1.6 prima aplicatie singur :D

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,59 +1,66 @@
-const Header = (props) => {
-  return (<div><h1>{props.course}</h1></div>)
-}
+import { useState } from 'react'
 
-const Part = (props) => {
+const Buton = ({ handleClick, text }) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
 }
 
-const Content = (props) => {
+const Give = (props) => {
   return (
     <div>
-      {props.parts.map(part =>
-        <Part key={part.name} part={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-  
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+      <h1>Give feedback</h1>
+      <Buton handleClick={props.handleGood} text="good" />
+      <Buton handleClick={props.handleNeutral} text="neutral" />
+      <Buton handleClick={props.handleBad} text="bad" />
     </div>
   )
 }
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    const newGood = good + 1
+    setGood(newGood)
+    console.log('Good button clicked', newGood)
+  }
+
+  const handleNeutral = () => {
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+    console.log('Neutral button clicked', newNeutral)
+  }
+
+  const handleBad = () => {
+    const newBad = bad + 1
+    setBad(newBad)
+    console.log('Bad button clicked', newBad)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Give handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
+
 export default App
