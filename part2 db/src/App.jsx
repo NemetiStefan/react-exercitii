ff23d0cbc6ca89b657b2c4f7b963bad32ffcb807
NemetiStefan <stefval112@gmail.com>
2025-07-03 10:36:39 +0300

    notes app cu fetch de peserver

--- part2 db/src/App.jsx
+++ part2 db/src/App.jsx
@@ -1,88 +1,58 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const [persons, setPersons] = useState([{
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    name: 'Nemeti Stefan',
-    number:'dfgn cfg',
-  },
-  {
-    name: 'Daria',
-    number: 'bhjmnb',
-  }
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-])
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
-  const [newName, setNewName] = useState('')
-
-  const [newNumber, setNewNumber] = useState('')
-
-  const [newFilter, setFilter] = useState('')
-
-
-  //functie care se ocupa efectiv de adaugarea in memorie
-  const addPerson = (event) => {
+  const addNote = (event) => {
     event.preventDefault()
-
-    if(persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      return
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
     }
 
-    if(newName === '') {
-      alert('Name cannot be empty')
-      return
-    }
-
-    if(newNumber === '') {
-      alert('Number cannot be empty')
-      return
-    }
-
-    const newPerson = {
-      name: newName,
-      number: newNumber
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  
-
-  //functia ca sa se vada schimbarea de nume in text box
-  const handleNewPerson = (event) => {
-    setNewName(event.target.value)
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
   }
 
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
-    <div> 
-      <h1>Phonebook</h1>
-      <p>Filter: <input value={newFilter} onChange={(e) => setFilter(e.target.value)} /></p>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        name: <input
-          value={newName}
-          onChange={handleNewPerson}/><br />
-        number: <input
-          value={newNumber}
-          onChange={handleNewNumber}/><br />
-        <button type="submit">add</button>
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
       </form>
-      <h2>Numbers</h2>
-      <Filter persons={persons} newFilter={newFilter} setFilter={setFilter} />
-
     </div>
   )
-
 }
 
-export default App 
+export default App
